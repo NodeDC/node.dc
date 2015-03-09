bb221da07bed1898ebf2d5688f51e0032c70ed02
Josh Finnie <josh.finnie@trackmaven.com>
2015-03-09 11:20:23 -0400

    updating to work with api key via environment variable.

--- routes/api/meetup.js
+++ routes/api/meetup.js
@@ -5,9 +5,11 @@
 
 var request = require('request');
 
+var meetupApiKey = process.env.MEETUPAPIKEY || "test";
+
 exports.meetups = function (req, res) {
     'use strict';
-    var url = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=node-dc&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=26710252&sig=dc51b92626c66cdedabf20d5c39aceb4720eb70a';
+    var url = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=node-dc&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&key=' + meetupApiKey;
     request.get(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.json(JSON.parse(body));
