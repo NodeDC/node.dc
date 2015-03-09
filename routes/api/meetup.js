4bc984050b70d5abcff0e4d438969c95de5d94f1
Josh Finnie <josh.finnie@trackmaven.com>
2015-03-09 09:44:59 -0400

    updating the meetup api to only show NodeDC meetups.

--- routes/api/meetup.js
+++ routes/api/meetup.js
@@ -7,7 +7,7 @@ var request = require('request');
 
 exports.meetups = function (req, res) {
     'use strict';
-    var url = 'http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=node-dc&desc=false&member_id=26710252&offset=0&format=json&page=20&fields=&sig_id=26710252&sig=f9a8c7e4076e5ed5825f06378a855415af3722a9';
+    var url = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=node-dc&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=26710252&sig=dc51b92626c66cdedabf20d5c39aceb4720eb70a';
     request.get(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.json(JSON.parse(body));
